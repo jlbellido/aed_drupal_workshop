57192e4a21c0e82b813272e1a6acf45cb9531b19
jlbellido <no-reply-215463256523@gmail.com>
2015-01-15 19:43:59 +0100

    Added Database API example and node rendered

--- templates/aed_drupal_workshop_url_parameter.tpl.php
+++ templates/aed_drupal_workshop_url_parameter.tpl.php
@@ -5,4 +5,14 @@
   <div class="container-body">
     <?php print t('Your param sanitized is : @param', array('@param' => $param)); ?>
   </div>
+  <?php if (!empty($node_rendered)): ?>
+    <div class="container-node">
+      <span>
+        <?php print t('The node rendered is : '); ?>
+      </span>
+      <div>
+        <?php print render($node_rendered); ?>
+      </div>
+    </div>
+  <?php endif;?>
 </div>
