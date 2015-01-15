946c7d586a527587654d987e5f202352cf93cd52
jlbellido <no-reply-215463256523@gmail.com>
2015-01-15 19:07:05 +0100

    Added theme with template

---
+++ templates/aed_drupal_workshop_url_parameter.tpl.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="container-header">
+    <?php print $header_text; ?>
+  </div>
+  <div class="container-body">
+    <?php print t('Your param sanitized is : @param', array('@param' => $param)); ?>
+  </div>
+</div>
